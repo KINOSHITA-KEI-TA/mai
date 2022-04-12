32e6d58b6f06fe0b03a3f70d3a63b7904366c0ff
きのしたけいた <67889594+KINOSHITA-KEI-TA@users.noreply.github.com>
2022-04-12 22:32:35 +0900

    ログイン機能追加

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/sample', 'App\Http\Controllers\SampleController@showPage');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
